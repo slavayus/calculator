2a878cf8eab76dc07563a1e081c52467588b80df
Vladislav <slavayus@gmail.com>
2018-05-07 16:05:38 +0300

    update layout. Moved sqrt button to portrait orientation

--- app/src/main/java/com/job/calculator/CalculatorFragment.java
+++ app/src/main/java/com/job/calculator/CalculatorFragment.java
@@ -210,16 +210,22 @@ public class CalculatorFragment extends Fragment {
             }
         });
 
-        view.findViewById(R.id.percent_button).setOnClickListener(new View.OnClickListener() {
+        view.findViewById(R.id.sqrt_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                operation = operation == null && isThereResult ? Operations.PERCENT : operation;
+                operation = null;
+                isThereResult = true;
+                isThereDot = false;
+                if ("".equals(currentNumber)) {
+                    currentNumber = String.valueOf(result);
+                }
+                result = Math.sqrt(Double.parseDouble(currentNumber));
 
-                calculateResult(getCurrentNumberAsNumber());
+                dataInTextView += "\u221a " + (Math.abs(Double.parseDouble(currentNumber)) == Double.POSITIVE_INFINITY ? String.valueOf(Double.parseDouble(currentNumber)) : putComma(String.valueOf(Double.parseDouble(currentNumber)))) +
+                        "\n = " + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n\n ";
 
                 currentNumber = "";
-                operation = Operations.PERCENT;
-                String newText = dataInTextView + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n" + operation;
+                String newText = dataInTextView + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n ";
                 textView.setText("");
                 textView.append(newText);
             }
@@ -247,43 +253,17 @@ public class CalculatorFragment extends Fragment {
                     textView.append(newText);
                 }
             });
-            view.findViewById(R.id.cube_button).setOnClickListener(new View.OnClickListener() {
+
+            view.findViewById(R.id.percent_button).setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    operation = null;
-                    isThereResult = true;
-                    isThereDot = false;
-                    if ("".equals(currentNumber)) {
-                        currentNumber = String.valueOf(result);
-                    }
-                    result = Math.pow(Double.parseDouble(currentNumber), 3);
+                    operation = operation == null && isThereResult ? Operations.PERCENT : operation;
 
-                    dataInTextView += (Math.abs(Double.parseDouble(currentNumber)) == Double.POSITIVE_INFINITY ? String.valueOf(Double.parseDouble(currentNumber)) : putComma(String.valueOf(Double.parseDouble(currentNumber)))) +
-                            "\n ^ 3 \n = " + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n\n ";
+                    calculateResult(getCurrentNumberAsNumber());
 
                     currentNumber = "";
-                    String newText = dataInTextView + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n ";
-                    textView.setText("");
-                    textView.append(newText);
-                }
-            });
-
-            view.findViewById(R.id.sqrt_button).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    operation = null;
-                    isThereResult = true;
-                    isThereDot = false;
-                    if ("".equals(currentNumber)) {
-                        currentNumber = String.valueOf(result);
-                    }
-                    result = Math.sqrt(Double.parseDouble(currentNumber));
-
-                    dataInTextView += "\u221a " + (Math.abs(Double.parseDouble(currentNumber)) == Double.POSITIVE_INFINITY ? String.valueOf(Double.parseDouble(currentNumber)) : putComma(String.valueOf(Double.parseDouble(currentNumber)))) +
-                            "\n = " + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n\n ";
-
-                    currentNumber = "";
-                    String newText = dataInTextView + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n ";
+                    operation = Operations.PERCENT;
+                    String newText = dataInTextView + (Math.abs(result) == Double.POSITIVE_INFINITY ? String.valueOf(result) : putComma(String.valueOf(result))) + "\n" + operation;
                     textView.setText("");
                     textView.append(newText);
                 }
