4100147b95f7bcc35cab57de03965245619b6b6a
Vladislav <slavayus@gmail.com>
2018-05-10 18:19:45 +0300

    add javadoc to temperature commands

--- app/src/main/java/com/job/calculator/commands/temperature/Fahrenheit.java
+++ app/src/main/java/com/job/calculator/commands/temperature/Fahrenheit.java
@@ -5,11 +5,24 @@ package com.job.calculator.commands.temperature;
  */
 
 public class Fahrenheit implements Temperature {
+
+    /**
+     * Convert from Celsius to Fahrenheit.
+     *
+     * @param param degrees in Celsius scale.
+     * @return degrees in Fahrenheit scale.
+     */
     @Override
     public double fromCelsius(double param) {
         return (param - 32) * 5 / 9;
     }
 
+    /**
+     * Convert from Fahrenheit to Celsius.
+     *
+     * @param param degrees in Fahrenheit scale.
+     * @return degrees in Celsius scale.
+     */
     @Override
     public double toCelsius(double param) {
         return param * 9 / 5 + 32;
